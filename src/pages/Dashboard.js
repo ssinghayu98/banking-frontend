b6e66dcddcf6304d44a9770b82bd0d768922cee0
Ayush Singh <ssinghayu98@gmail.com>
2026-05-03 17:08:03 +0530

    fix imports using absolute path

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API_URL from "./api";
+import API_URL from "api";
 
 function Dashboard() {
   const [balance, setBalance] = useState(0);
